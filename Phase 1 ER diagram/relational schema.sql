b655a1e9258a090f4a226e09ae3b5781e439fb77
StevenHuo <ioshuoyuhao@gmail.com>
2023-02-03 21:16:54 -0500

    Update relational schema.sql
    revision on sql statements, beFriend relation still has error

--- Phase 1 ER diagram/relational schema.sql
+++ Phase 1 ER diagram/relational schema.sql
@@ -2,95 +2,95 @@ CREATE DATABASE PA1;
 use PA1;
 
 CREATE TABLE Users ( -- capitalized entitys for notations
-	uid VARCHAR(20),
-	first_ame VARCHAR(10),
-	last_name VARCHAR(10),
-    email VARCHAR(20) unique,
+	uid INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(10) NOT NULL, -- user must have a name
+	last_name VARCHAR(10) NOT NULL,
+    email VARCHAR(20) NOT NULL, -- user need to regiester with a email
     job VARCHAR(10),
-    howmtown VARCHAR(20),
+    hometown VARCHAR(20),
     gender VARCHAR(20),
-    password VARCHAR(20),
-    PRIMARY KEY (uid)
+    password VARCHAR(20)
 );
 
 CREATE TABLE be_friend(  -- no capitalized refers to relationships
-	uid VARCHAR(20),
-	PRIMARY KEY (uid),
-    fOREIGN KEY (uid) REFERENCES Users(uid)
+	uid1 INT, 
+    uid2 INT,
+    PRIMARY KEY (uid, uid2),
+    FOREIGN KEY (uid1) REFERENCES Users(uid),
+    FOREIGN KEY (uid2) REFERENCES Users(uid) -- users can be friends of other users (many to many)
 );
+ -- ALTER TABLE be_friend ADD INDEX(uid1);
+ -- ALTER TABLE be_friend CHANGE uid1 uid1 INT AUTO_INCREMENT;
 
 CREATE TABLE Albums(
-	aid VARCHAR(20),
+	aid INT PRIMARY KEY AUTO_INCREMENT, 
 	album_name VARCHAR(20),
-    owner_id VARCHAR(20),
-    date_creation date,
-    PRIMARY KEY (aid)
+    owner_id INT, 
+    date_creation date
 );
 
-CREATE TABLE creating(
-	uid VARCHAR(20) not null,
-    aid VARCHAR(20),
-    primary key (uid),   
-    foreign key (aid) references Albums(aid) on delete cascade,
-    foreign key (uid) references Users(uid) on delete cascade
+CREATE TABLE creating(  -- for each albums, it should be created by only one user. 
+	uid INT NOT NULL,  -- total participation constraint that for each albums, it need has at least one user as owner.
+    aid INT  PRIMARY KEY,-- aid is unique identify this relationship due to key constraint that for each album, it should have one user at most
+    FOREIGN KEY (aid) references Albums(aid) ON DELETE NO ACTION, 
+    FOREIGN KEY (uid) references Users(uid) ON DELETE NO ACTION
 );
 
 CREATE TABLE Photos(
-	pid VARCHAR(20),
+	pid INT PRIMARY KEY AUTO_INCREMENT,
     data_photo VARCHAR(255),
-    caption VARCHAR(20),
-    primary key (pid)
+    caption VARCHAR(20)
 );
 
 CREATE TABLE Tags(
-	word VARCHAR(20),
-    primary key (word)
+	word INT PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE associate(
-	pid VARCHAR(20),
-    word VARCHAR(20),
-    primary key (pid, word),
-    foreign key (pid) references Photos(pid),
-    foreign key (word) references Tags(word)
+	pid INT,
+    word INT,
+    PRIMARY KEY (pid, word),
+    FOREIGN KEY (pid) REFERENCES Photos(pid),
+    FOREIGN KEY (word) REFERENCES Tags(word)
 );
 
-CREATE TABLE contain(
-	aid VARCHAR(20) not null ,
-    pid VARCHAR(20),
-    primary key (aid),  -- key constraint that for each photo only stored in one album
-    foreign key (pid) references Photos(pid) on delete cascade
+CREATE TABLE contain(  -- for each photos, it should be contained in only one album
+	aid INT NOT NULL,-- total participation constraint that for each photo it should be contained at least one album
+    pid INT PRIMARY KEY, -- key constraint that each photo can have at most one album
+	FOREIGN KEY (pid) REFERENCES Photos(pid) ON DELETE NO ACTION,
+    FOREIGN KEY (aid) REFERENCES  Albums(aid) ON DELETE NO ACTION
 );
 
 CREATE TABLE likes(
-	uid VARCHAR(20),
-    pid VARCHAR(20),
-    primary key (uid, pid),
-    foreign key (uid) references Users(uid),
-    foreign key (pid) references Photos(pid)
+	uid INT,
+    pid INT,
+    PRIMARY KEY (uid, pid),
+    FOREIGN KEY (uid) REFERENCES Users(uid),
+    FOREIGN KEY (pid) REFERENCES Photos(pid)
 );
 
 CREATE TABLE Comments(
-	cid VARCHAR(20),
+	cid INT PRIMARY KEY AUTO_INCREMENT,
     text VARCHAR(255),
-    owner_id VARCHAR(20),
-    date_comment date,
-    primary key (cid)
+    owner_id INT,
+    date_comment date
 );
 
-CREATE TABLE comment_on(
-	pid VARCHAR(20) not null,
-    cid VARCHAR(20),
-    primary key (pid), -- key constrant that for each comments, it can only comments on one single photo
-    foreign key (cid) references Comments(cid) on update cascade
+CREATE TABLE comment_on( -- each comments should related to only one photo
+	pid INT NOT NULL, -- participation constraint
+    cid INT PRIMARY KEY, -- KEY constraint so cid is unique to identify this relationship
+    FOREIGN KEY (pid) REFERENCES photos(pid) ON DELETE NO ACTION,
+    FOREIGN KEY (cid) REFERENCES Comments(cid) ON DELETE NO ACTION
 );
 
-CREATE TABLE creates(
-	uid VARCHAR(20) not null ,
-    cid VARCHAR(20),
-    primary key (uid), -- key contraints that for each comments, it can only be created by one user.
-    foreign key (cid) references Comments(cid) on delete cascade
+CREATE TABLE creates( -- comments for each should be created by only one user
+	uid INT NOT NULL,
+    cid INT PRIMARY KEY, -- key contraints that for each comments, it can only be created by onw user.
+    FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE NO ACTION,
+    FOREIGN KEY (cid) REFERENCES Comments(cid) ON DELETE NO ACTION
 );
 
     
+    
+
     
