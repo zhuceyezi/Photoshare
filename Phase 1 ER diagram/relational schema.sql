85117217e171e37db508d285a51b258fbf6f92e4
zhuceyezi <hln1234567890@qq.com>
2023-02-06 16:26:42 -0500

    fixed schema

--- Phase 1 ER diagram/relational schema.sql
+++ Phase 1 ER diagram/relational schema.sql
@@ -1,23 +1,24 @@
-CREATE DATABASE PA1;
+CREATE DATABASE IF NOT exists PA1;
 use PA1;
 
 CREATE TABLE Users ( -- capitalized entitys for notations
-	uid INT PRIMARY KEY AUTO_INCREMENT,
-	first_name VARCHAR(10) NOT NULL, -- user must have a name
-	last_name VARCHAR(10) NOT NULL,
-    email VARCHAR(20) NOT NULL, -- user need to regiester with a email
+	uid INT AUTO_INCREMENT,
+	first_name VARCHAR(20) NOT NULL, -- user must have a name
+	last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL, -- user need to register with a email
     job VARCHAR(10),
     hometown VARCHAR(20),
     gender VARCHAR(20),
-    password VARCHAR(20)
+    user_password VARCHAR(20) NOT NULL,
+    PRIMARY KEY (uid)
 );
 
-CREATE TABLE be_friend(  -- no capitalized refers to relationships
-	uid1 INT, 
-    uid2 INT,
-    PRIMARY KEY (uid, uid2),
-    FOREIGN KEY (uid1) REFERENCES Users(uid),
-    FOREIGN KEY (uid2) REFERENCES Users(uid) -- users can be friends of other users (many to many)
+CREATE TABLE be_friend(
+	uid_from INT, 
+    uid_to INT,
+    PRIMARY KEY (uid_from, uid_to),
+    FOREIGN KEY (uid_to) REFERENCES Users(uid),
+	FOREIGN KEY (uid_from) REFERENCES Users(uid)
 );
  -- ALTER TABLE be_friend ADD INDEX(uid1);
  -- ALTER TABLE be_friend CHANGE uid1 uid1 INT AUTO_INCREMENT;
@@ -29,39 +30,39 @@ CREATE TABLE Albums(
     date_creation date
 );
 
-CREATE TABLE creating(  -- for each albums, it should be created by only one user. 
+CREATE TABLE create_album(  -- for each albums, it should be created by only one user. 
 	uid INT NOT NULL,  -- total participation constraint that for each albums, it need has at least one user as owner.
-    aid INT  PRIMARY KEY,-- aid is unique identify this relationship due to key constraint that for each album, it should have one user at most
-    FOREIGN KEY (aid) references Albums(aid) ON DELETE NO ACTION, 
-    FOREIGN KEY (uid) references Users(uid) ON DELETE NO ACTION
+    aid INT PRIMARY KEY,-- aid is unique identify this relationship due to key constraint that for each album, it should have one user at most
+    FOREIGN KEY (aid) references Albums(aid), 
+    FOREIGN KEY (uid) references Users(uid)
 );
 
 CREATE TABLE Photos(
 	pid INT PRIMARY KEY AUTO_INCREMENT,
-    data_photo VARCHAR(255),
-    caption VARCHAR(20)
+    data_photo LONGBLOB, -- store data in binary
+    caption VARCHAR(255)
 );
 
 CREATE TABLE Tags(
-	word INT PRIMARY KEY AUTO_INCREMENT
+	word PRIMARY KEY VARCHAR(25) 
 );
 
 CREATE TABLE associate(
 	pid INT,
-    word INT,
+    word VARCHAR(25),
     PRIMARY KEY (pid, word),
     FOREIGN KEY (pid) REFERENCES Photos(pid),
     FOREIGN KEY (word) REFERENCES Tags(word)
 );
 
-CREATE TABLE contain(  -- for each photos, it should be contained in only one album
+CREATE TABLE album_contain_photo(  -- for each photos, it should be contained in only one album
 	aid INT NOT NULL,-- total participation constraint that for each photo it should be contained at least one album
     pid INT PRIMARY KEY, -- key constraint that each photo can have at most one album
-	FOREIGN KEY (pid) REFERENCES Photos(pid) ON DELETE NO ACTION,
-    FOREIGN KEY (aid) REFERENCES  Albums(aid) ON DELETE NO ACTION
+	FOREIGN KEY (pid) REFERENCES Photos(pid),
+    FOREIGN KEY (aid) REFERENCES  Albums(aid)
 );
 
-CREATE TABLE likes(
+CREATE TABLE user_like_photo(
 	uid INT,
     pid INT,
     PRIMARY KEY (uid, pid),
@@ -71,23 +72,23 @@ CREATE TABLE likes(
 
 CREATE TABLE Comments(
 	cid INT PRIMARY KEY AUTO_INCREMENT,
-    text VARCHAR(255),
-    owner_id INT,
+    content VARCHAR(255),
+    owner_id INT NOT NULL,
     date_comment date
 );
 
-CREATE TABLE comment_on( -- each comments should related to only one photo
+CREATE TABLE comments_comment_on_photo( -- each comments should related to only one photo
 	pid INT NOT NULL, -- participation constraint
     cid INT PRIMARY KEY, -- KEY constraint so cid is unique to identify this relationship
-    FOREIGN KEY (pid) REFERENCES photos(pid) ON DELETE NO ACTION,
-    FOREIGN KEY (cid) REFERENCES Comments(cid) ON DELETE NO ACTION
+    FOREIGN KEY (pid) REFERENCES photos(pid),
+    FOREIGN KEY (cid) REFERENCES Comments(cid)
 );
 
-CREATE TABLE creates( -- comments for each should be created by only one user
+CREATE TABLE user_create_comment( -- comments for each should be created by only one user
 	uid INT NOT NULL,
     cid INT PRIMARY KEY, -- key contraints that for each comments, it can only be created by onw user.
-    FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE NO ACTION,
-    FOREIGN KEY (cid) REFERENCES Comments(cid) ON DELETE NO ACTION
+    FOREIGN KEY (uid) REFERENCES Users(uid),
+    FOREIGN KEY (cid) REFERENCES Comments(cid)
 );
 
     
