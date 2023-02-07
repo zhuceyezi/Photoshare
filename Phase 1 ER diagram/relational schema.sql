f48580c97ed84bd2f5ff394f43aeed4fe7d4a721
zhuceyezi <hln1234567890@qq.com>
2023-02-07 12:11:37 -0500

    additional fix

--- Phase 1 ER diagram/relational schema.sql
+++ Phase 1 ER diagram/relational schema.sql
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS Comments CASCADE;
 
 
 CREATE TABLE Users ( -- capitalized entitys for notations
-	user_id INT44 AUTO_INCREMENT,
+	user_id INT4 AUTO_INCREMENT,
 	first_name VARCHAR(20),
 	last_name VARCHAR(20),
     email VARCHAR(30) UNIQUE,
@@ -28,8 +28,8 @@ CREATE TABLE Users ( -- capitalized entitys for notations
 );
 
 CREATE TABLE be_friend(
-	user_id_from INT44, 
-    user_id_to INT44,
+	user_id_from INT4, 
+    user_id_to INT4,
     PRIMARY KEY (user_id_from, user_id_to),
     FOREIGN KEY (user_id_to) REFERENCES Users(user_id),
 	FOREIGN KEY (user_id_from) REFERENCES Users(user_id)
@@ -38,22 +38,22 @@ CREATE TABLE be_friend(
  -- ALTER TABLE be_friend CHANGE user_id1 user_id1 INT4 AUTO_INCREMENT;
 
 CREATE TABLE Albums(
-	album_id INT44 PRIMARY KEY AUTO_INCREMENT, 
+	album_id INT4 PRIMARY KEY AUTO_INCREMENT, 
 	album_name VARCHAR(20),
-    owner_id INT44, 
+    owner_id INT4, 
     date_creation date
 );
 
 CREATE TABLE create_album(  -- for each albums, it should be created by only one user. 
-	user_id INT44 NOT NULL,  
-    album_id INT44 PRIMARY KEY,
+	user_id INT4 NOT NULL,  
+    album_id INT4 PRIMARY KEY,
     FOREIGN KEY (album_id) references Albums(album_id), 
     FOREIGN KEY (user_id) references Users(user_id)
 );
 
 CREATE TABLE Pictures(
-	picture_id INT44 AUTO_INCREMENT,
-    user_id INT44,
+	picture_id INT4 AUTO_INCREMENT,
+    user_id INT4,
     imgdata LONGBLOB, -- store data in binary
     caption VARCHAR(255),
     INDEX upicture_id_idx (user_id),
@@ -65,7 +65,7 @@ CREATE TABLE Tags(
 );
 
 CREATE TABLE associate(
-	picture_id INT44,
+	picture_id INT4,
     word VARCHAR(25),
     PRIMARY KEY (picture_id, word),
     FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
@@ -73,15 +73,15 @@ CREATE TABLE associate(
 );
 
 CREATE TABLE album_contain_picture(  -- for each Pictures, it should be contained in only one album
-	album_id INT44 NOT NULL,
-    picture_id INT44 PRIMARY KEY, 
+	album_id INT4 NOT NULL,
+    picture_id INT4 PRIMARY KEY, 
 	FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
     FOREIGN KEY (album_id) REFERENCES  Albums(album_id)
 );
 
 CREATE TABLE user_like_picture(
-	user_id INT44,
-    picture_id INT44,
+	user_id INT4,
+    picture_id INT4,
     PRIMARY KEY (user_id, picture_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id)
@@ -95,7 +95,7 @@ CREATE TABLE Comments(
 );
 
 CREATE TABLE comments_comment_on_picture( -- each comments should related to only one photo
-	picture_id INT44 NOT NULL, 
+	picture_id INT4 NOT NULL, 
     comment_id INT4 PRIMARY KEY, 
     FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
     FOREIGN KEY (comment_id) REFERENCES Comments(comment_id)
