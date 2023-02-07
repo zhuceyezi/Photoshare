38c3b218fed106d66e5c959ed928f154d0cb82e8
zhuceyezi <hln1234567890@qq.com>
2023-02-07 12:55:06 -0500

    fix

--- schema.sql
+++ schema.sql
@@ -1,26 +1,21 @@
 CREATE DATABASE IF NOT exists PA1;
 use PA1;
 DROP TABLE IF EXISTS user_create_comment CASCADE;
-DROP TABLE IF EXISTS comments_comment_on_picture CASCADE;
-DROP TABLE IF EXISTS album_contain_picture CASCADE;
-DROP TABLE IF EXISTS user_like_picture CASCADE;
+DROP TABLE IF EXISTS user_like_Photo CASCADE;
 DROP TABLE IF EXISTS be_friend CASCADE;
-DROP TABLE IF EXISTS create_album CASCADE;
 DROP TABLE IF EXISTS associate CASCADE;
-DROP TABLE IF EXISTS Pictures CASCADE;
-DROP TABLE IF EXISTS Users CASCADE;
-DROP TABLE IF EXISTS Albums CASCADE;
 DROP TABLE IF EXISTS Tags CASCADE;
 DROP TABLE IF EXISTS Comments CASCADE;
-
-
+DROP TABLE IF EXISTS Photos CASCADE;
+DROP TABLE IF EXISTS Albums CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users ( -- capitalized entitys for notations
 	user_id INT4 AUTO_INCREMENT,
 	first_name VARCHAR(20),
 	last_name VARCHAR(20),
     email VARCHAR(30) UNIQUE,
-    job VARCHAR(10),
+    job VARCHAR(255),
     hometown VARCHAR(20),
     gender VARCHAR(20),
     password VARCHAR(255),
@@ -38,21 +33,24 @@ CREATE TABLE be_friend(
  -- ALTER TABLE be_friend CHANGE user_id1 user_id1 INT4 AUTO_INCREMENT;
 
 CREATE TABLE Albums(
-	album_id INT4 PRIMARY KEY AUTO_INCREMENT, 
+    album_id INT4 PRIMARY KEY AUTO_INCREMENT, 
 	album_name VARCHAR(255),
-  user_id INT4 NOT NULL, 
-  date_created date
+    user_id INT4 NOT NULL, 
+    date_created date,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 
-CREATE TABLE Pictures(
-	picture_id INT4 AUTO_INCREMENT,
+CREATE TABLE Photos(
+  photo_id INT4 AUTO_INCREMENT,
   user_id INT4 NOT NULL,
   album_id INT4 NOT NULL,
   imgdata LONGBLOB, -- store data in binary
   caption VARCHAR(255),
-  INDEX upicture_id_idx (user_id),
-  CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
+  INDEX uphoto_id_idx (user_id),
+  CONSTRAINT photos_pk PRIMARY KEY (photo_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (album_id) REFERENCES Albums(album_id)
 );
 
 CREATE TABLE Tags(
@@ -60,30 +58,31 @@ CREATE TABLE Tags(
 );
 
 CREATE TABLE associate(
-	picture_id INT4,
+	photo_id INT4,
     word VARCHAR(25),
-    PRIMARY KEY (picture_id, word),
-    FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id),
+    PRIMARY KEY (photo_id, word),
+    FOREIGN KEY (photo_id) REFERENCES Photos(photo_id),
     FOREIGN KEY (word) REFERENCES Tags(word)
 );
 
 
-CREATE TABLE user_like_picture(
+CREATE TABLE user_like_Photo(
 	user_id INT4,
-    picture_id INT4,
-    PRIMARY KEY (user_id, picture_id),
+    photo_id INT4,
+    PRIMARY KEY (user_id, photo_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (picture_id) REFERENCES Pictures(picture_id)
+    FOREIGN KEY (photo_id) REFERENCES Photos(photo_id)
 );
 
 CREATE TABLE Comments(
 	comment_id INT4 PRIMARY KEY AUTO_INCREMENT,
+    user_id INT4 NOT NULL,
+    photo_id INT4 NOT NULL,
     content VARCHAR(255),
-    owner_id INT4 NOT NULL,
-    date_comment date
+    date_comment date,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (photo_id) REFERENCES Photos(photo_id)
 );
-
-
 
 INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
 INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
